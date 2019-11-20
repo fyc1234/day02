e6d18481ffcf068c80017669fc5ade62cd87fcf1
fyc1234 <2450182061@qq.com>
2019-11-20 20:59:40 +0800

    题目3

--- app/src/main/java/com/example/fragment/FavorateFragment.java
+++ app/src/main/java/com/example/fragment/FavorateFragment.java
@@ -1,12 +1,15 @@
 package com.example.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,18 +38,23 @@ public class FavorateFragment extends Fragment {
         return view;
     }
 
+
     private void initView(View view) {
         mRv = (RecyclerView) view.findViewById(R.id.rv);
         mRv.setLayoutManager(new LinearLayoutManager(getContext()));
         datas = new ArrayList<>();
         adapter = new RvAdapter(datas,getContext());
         mRv.setAdapter(adapter);
+
+
+
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         if (isVisibleToUser){
+            datas.clear();
             datas.addAll(dao.loadAll());
             adapter.notifyDataSetChanged();
         }
